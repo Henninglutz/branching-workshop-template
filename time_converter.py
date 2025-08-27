e5cc58871d9022d3cd60d2dd50f449caae373141
Stefan S. <thekyoshalp@live.de>
2025-08-27 13:30:47 +0200

    create file

---
+++ time_converter.py
@@ -0,0 +1,11 @@
+def seconds_to_minutes(seconds: int) -> float:
+    """
+    Convert seconds to minutes.
+
+    Args:
+        seconds (int): Number of seconds.
+
+    Returns:
+        float: Equivalent time in minutes.
+    """
+    return seconds / 60
